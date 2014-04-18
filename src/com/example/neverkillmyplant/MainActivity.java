6ee0b96a2f3cd6d5a29dce52e69259e3a6d2f353
Jiliac <v.manes94@gmail.com>
2014-04-18 11:54:00 +0200

    Module ANDROID: On affiche maintenant les plantes et leur données grâce à des expandable list. Il faut améliorer le rendu. Il faut ajouter un bouton diagnostique qui n'est plus accessible.

--- src/com/example/neverkillmyplant/MainActivity.java
+++ src/com/example/neverkillmyplant/MainActivity.java
@@ -13,6 +13,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -20,22 +21,19 @@ import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
-	private PlantArray planteListe;
-	private PlantArray plantesAffichees;
+	private PlantArray planteListe = new PlantArray();
 
-	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// Creation de la page de base
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// cree le dossier de stockage si necessaire
+		createDirectory();
+
+		// la police
 		Typeface FONT = Typeface.createFromAsset(getAssets(),
 				"fonts/MoonFlower.ttf");
-
-		// les boutons ont besoin d'être réaffiché.
-		planteListe = new PlantArray();
-		plantesAffichees = new PlantArray();
-		plantesAffichees.save("neverkillmyplant" + File.separator
-				+ "affichage.data");
 
 		// on set la methode du bouton add
 		Button addPlanButton = (Button) findViewById(R.id.button1);
@@ -56,89 +54,55 @@ public class MainActivity extends Activity {
 	public void onPause() {
 		super.onPause();
 		planteListe.save("neverkillmyplant" + File.separator + "liste.data");
-		plantesAffichees.save("neverkillmyplant" + File.separator
-				+ "affichage.data");
 	}
 
 	public void onResume() {
 		super.onResume();
 
 		planteListe.load("neverkillmyplant" + File.separator + "liste.data");
-		
-		 plantesAffichees.load("neverkillmyplant" + File.separator +
-		 "affichage.data"); this.ajoutBouton();
-		
 		handleListView((ExpandableListView) findViewById(R.id.expandableListView1));
 	}
 
-	/************************** methode auxiliaires ************************/
-
-	public void ajoutBouton() {
-		// on crée dynamiquement un bouton par l'intermédiaire d'un layout
-		LinearLayout layoutOfDynamicContent = (LinearLayout) findViewById(R.id.layout);
-		LinearLayout A = new LinearLayout(this);
-		A.setOrientation(LinearLayout.VERTICAL);
-
-		for (Plant plant : planteListe) {
-			if (!plantesAffichees.isIn(plant)) {
-				plantesAffichees.add(plant);
-
-				// on crée
-				Button bouton = new Button(this);
-				bouton.setText(plant.getName());
-				// on affiche
-				A.addView(bouton);
-				// on va vers la fiche de la plante lors du click
-				bouton.setOnClickListener(new clickPlantButton(plant, this));
-				Typeface FONT = Typeface.createFromAsset(getAssets(),
-						"fonts/MoonFlower.ttf");
-				bouton.setTypeface(FONT);
-			}
-		}
-		layoutOfDynamicContent.addView(A);
-	}
-
-	// TEST DES LISTVIEW
 	public void handleListView(ExpandableListView view) {
 		ArrayList<String> titres = new ArrayList<String>();
 		HashMap<String, List<String[]>> fils = new HashMap<String, List<String[]>>();
-		
-		titres.add("premier titre");
-		titres.add("second titre");
-		
-		List<String[]> premier = new ArrayList<String[]>();
-		String[] str1 = {"ADRESSE","1.1","1.2"};
-		premier.add(str1);
-		
-		
-		List<String[]> second = new ArrayList<String[]>();
-		String[] str2 = {"ADRESSE","2.1","2.2"};
-		second.add(str2);
-		
-		fils.put("premier titre", premier);
-		fils.put("second titre", second);
-		
-		
-		ExpandableListAdapter adapter = new ExpandableListAdapter(this,titres,fils);
+
+		for (Plant plant : planteListe) {
+			titres.add(plant.getName());
+
+			List<String[]> plantAttributs = new ArrayList<String[]>();
+			String[] str = new String[3];
+			str[0] = "ADRESSE";
+			str[1] = "Espece : " + plant.getEspece();
+			str[2] = "Sante : " + "A RECUPER";
+			plantAttributs.add(str);
+
+			fils.put(plant.getName(), plantAttributs);
+		}
+
+		ExpandableListAdapter adapter = new ExpandableListAdapter(this, titres,
+				fils);
 		view.setAdapter(adapter);
 	}
-	
+
 	/********************************************************************************/
 
 	// methode de debug
 	public void reset() {
 		planteListe = new PlantArray();
-		plantesAffichees = new PlantArray();
 
 		planteListe.save("neverkillmyplant" + File.separator + "liste.data");
-		plantesAffichees.save("neverkillmyplant" + File.separator
-				+ "affichage.data");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	// methode auxiliaire
+	private void createDirectory() {
+		String sdCardDir = Environment.getExternalStorageDirectory()
+				.getAbsolutePath();
+		File mainDirectory = new File(sdCardDir + File.separator
+				+ "neverkillmyplant");
+		if (!mainDirectory.isDirectory()) {
+			File appDirectory = new File(sdCardDir, "neverkillmyplant");
+			appDirectory.mkdir();
+		}
 	}
 }
